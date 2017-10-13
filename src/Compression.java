2e16e65b618a9fe9c61ef042fba1757d9d997a2a
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2017-10-13 13:32:02 +0200

    Initial commit

---
+++ src/Compression.java
@@ -0,0 +1,112 @@
+public class Compression {
+
+    public Compression(){
+
+    }
+
+    /**
+     * Used to encode numbers and repeated chars
+     * Numbers as input char will be encoded to Hex 0x00 - 0x09
+     * @param in Input char to be encoded
+     * @param num How often the char is repeated
+     * @return Encoded array of chars
+     */
+    private char[] encode(char in, int num){
+        String numAsChar = "";
+        char[] out = new char[1];
+        if(num > 0) {
+            numAsChar = String.valueOf(num);
+            out = new char[numAsChar.length() + 1];
+        }
+
+        char outChar = in;
+        if(in <= '9' && in >= '0'){
+            outChar = (char)((int) in - 48);
+        }
+        out[0] = outChar;
+        for(int i = 1; i < out.length; i++) {
+            out[i] = numAsChar.charAt(i-1);
+        }
+        return out;
+    }
+
+    /**
+     * For compressing a text string containing numbers, signs and chars
+     * @param input Input String to be compressed
+     * @return Compressed String
+     */
+    public String compress(String input){
+        StringBuilder compressedString = new StringBuilder();
+        int numberOfSameChars = 0;
+        int strLen = input.length();
+
+        for(int i = 0; i < strLen; i++){
+
+            if(i < (strLen-1) && input.charAt(i) == input.charAt(i+1)){
+                numberOfSameChars++;
+            } else {
+                char[] encodedChars = encode(input.charAt(i), numberOfSameChars);
+                for (int j = 0; j < encodedChars.length; j++) {
+                    compressedString.append(encodedChars[j]);
+                }
+                numberOfSameChars = 0;
+            }
+        }
+        return compressedString.toString();
+    }
+
+    /**
+     * Check if a char is Number in ASCII
+     * @param asciiChar Input char
+     * @return true if the char is a number
+     */
+    private boolean isAsciiNum(char asciiChar){
+        if(asciiChar <= '9' && asciiChar >= '0'){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Decode numbers for decompression
+     * @param input input char to be decoded
+     * @return Decoded char
+     */
+    private char decodeNumbers(char input){
+        if(input <= 9){
+            return ((char)((int)input + 48));
+        }
+        return input;
+    }
+
+    /**
+     * Decompress a string previously compressed with the function in this class
+     * @param input Input compressed string
+     * @return Decompressed text
+     */
+    public String decompress(String input){
+        StringBuilder decompressedString = new StringBuilder();
+        String numOfChars = "";
+        int strLen = input.length();
+
+        for(int i = 0; i < strLen; i++){
+            if(i < (strLen-1) && isAsciiNum(input.charAt(i+1))) {
+                numOfChars += input.charAt(i+1);
+            } else {
+                int intNumOfChars;
+                if(numOfChars.isEmpty()){
+                    intNumOfChars = 0;
+                } else {
+                    intNumOfChars = Integer.parseInt(numOfChars);
+                }
+                for(int j = 0; j < intNumOfChars+1; j++){
+                    decompressedString.append(decodeNumbers(input.charAt(i-numOfChars.length())));
+                }
+                numOfChars = "";
+            }
+        }
+        return decompressedString.toString();
+    }
+
+
+}
---
+++ src/SearchAlgorithm.java
@@ -0,0 +1,152 @@
+import java.util.Random;
+
+public class SearchAlgorithm {
+
+    private Random rnd = new Random();
+
+    public SearchAlgorithm(){
+
+    }
+
+    /**
+     * Simple iterative binary search
+     * @param values Sorted array with integer values
+     * @param key Integer key to find in values
+     * @return Array index of key in values
+     */
+    public int binarySearch(int[] values, int key){
+        int start = 0, end = values.length - 1;
+        int i = -1;
+        while(i == -1 && start <= end){
+            int mid = (start + end) / 2;
+            if(values[mid] == key){
+                i = mid;
+            } else if(key < values[mid]){
+                end = mid -1 ;
+            } else if(key > values[mid]){
+                start = mid + 1;
+            }
+        }
+        return i;
+    }
+
+    /**
+     * Search string by char starting from the left side of the string
+     * @param searchStr String to be searched
+     * @param key Character to be found in searchStr
+     * @return Index of key in searchStr
+     */
+    public int findCharLeft(String searchStr, char key){
+        int pos = -1;
+
+        for(int i = 0; i < searchStr.length(); i++){
+            if(searchStr.charAt(i) == key){
+                pos = i;
+                break;
+            }
+        }
+        return pos;
+    }
+
+    /**
+     * Search string starting from the right side of the string
+     * @param searchStr String to be searched
+     * @param key Character to be found in searchStr
+     * @return Index of key in searchStr
+     */
+    public int findCharRight(String searchStr, char key){
+        int pos = -1;
+
+        for(int i = searchStr.length()-1; i >= 0; i--){
+            if(searchStr.charAt(i) == key){
+                pos = i;
+                break;
+            }
+        }
+        return pos;
+    }
+
+    /**
+     * Find char starting at a random position
+     * @param searchStr String to be searched
+     * @param key Character to be found in searchStr
+     * @return Index of key in searchStr
+     */
+    public int findCharRandom(String searchStr, char key){
+
+        int rndPos = rnd.nextInt(searchStr.length()-1);
+        int i = rndPos;
+        System.out.println("rnd pos: " + rndPos);
+        while(searchStr.charAt(i) != key){
+            i++;
+            i%=searchStr.length();
+            if(i == rndPos){
+                i = -1;
+                break;
+            }
+        }
+        return i;
+    }
+
+    public int[] selectionSort(int[] input){
+
+        for(int i = 0; i < input.length; i++){
+            int min = i;
+            for(int j = i+1; j < input.length; j++){
+                if(input[j] < input[min]){
+                    min = j;
+                }
+            }
+            int temp = input[i];
+            input[i] = input[min];
+            input[min] = temp;
+        }
+        return input;
+    }
+
+    public int[] insertionSort(int[] input){
+        for(int i = 1; i < input.length; i++){
+            int x = input[i];
+            int j = i;
+            while(j > 0 && input[j-1] > x){
+                input[j] = input[j-1];
+                j--;
+            }
+            input[j] = x;
+        }
+        return input;
+    }
+
+    public int[] quickSort(int[] values, int lb, int ub){
+        partition(values, 0, values.length-1);
+
+        if(lb > ub){
+            //return
+        }
+        return null;
+
+    }
+
+    public int partition(int[] inputValues, int lowerBound, int upperBound){
+        int len = inputValues.length;
+        int pivot = inputValues[(lowerBound+upperBound) / 2];
+        int foundElementLeft, foundElementRight;
+
+        while(lowerBound < upperBound) {
+            while (inputValues[lowerBound] < pivot) {
+                lowerBound++;
+            }
+            while(inputValues[upperBound] > pivot){
+                upperBound--;
+            }
+            if(lowerBound < upperBound){
+                int temp = inputValues[lowerBound];
+                inputValues[lowerBound] = inputValues[upperBound];
+                inputValues[upperBound] = temp;
+                lowerBound++;
+                upperBound--;
+            }
+        }
+        return lowerBound;
+    }
+}
